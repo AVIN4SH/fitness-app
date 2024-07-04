df39ed1791eaa98d29877c4ed336ac66553ba94b
Avinash <savinashkumarsingh619@gmail.com.com>
2024-07-04 12:21:29 +0530

    COMMENTED DATA FETCHING & API AS LIMIT REACHED

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,28 +1,30 @@
-export const exerciseOptions = {
-  method: "GET",
-  port: null,
-  path: "/status",
-  headers: {
-    "x-rapidapi-key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
-    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-  },
-};
+//API Limit Reacher
 
-export const youtubeOptions = {
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
-    "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
-  },
-};
+// export const exerciseOptions = {
+//   method: "GET",
+//   port: null,
+//   path: "/status",
+//   headers: {
+//     "x-rapidapi-key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+//     "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+//   },
+// };
 
-export const fetchData = async (url, options) => {
-  try {
-    const response = await fetch(url, options);
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-    return "No Relevant Data Found";
-  }
-};
+// export const youtubeOptions = {
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+//     "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
+//   },
+// };
+
+// export const fetchData = async (url, options) => {
+//   try {
+//     const response = await fetch(url, options);
+//     const data = await response.json();
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//     return "No Relevant Data Found";
+//   }
+// };
